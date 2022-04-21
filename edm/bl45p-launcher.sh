645479b3729923a0d773d1c17abd1684ec5087c7
Giles Knap <giles.knap@diamond.ac.uk>
2022-04-21 14:01:33 +0100

    bumping mo-ioc-01 generic IOC version

--- edm/bl45p-launcher.sh
+++ edm/bl45p-launcher.sh
@@ -8,29 +8,30 @@ thisdir=$(realpath $(dirname ${BASH_SOURCE[0]}))
 # there is a cagateway running on p45-ws001 at 172.23.59.64
 export EPICS_CA_ADDR_LIST=${EPICS_CA_ADDR_LIST:-172.23.59.64}
 
-if [ ! -z $(which edm 2>  /dev/null) ]
-then
-    export EDMDATAFILES=$(echo $EDMDATAFILES | sed s+/screens+${thisdir}+g)
-    echo launching native edm with paths: $EDMDATAFILES
-    edm -noedit -x ${start}
-    exit 0
-fi
+# if there is a local install of edm run it (deprecated for RHEL8 !!)
+#
+# if [ ! -z $(which edm 2>  /dev/null) ]
+# then
+#     export EDMDATAFILES=$(echo $EDMDATAFILES | sed s+/screens+${thisdir}+g)
+#     echo launching native edm with paths: $EDMDATAFILES
+#     edm -noedit -x ${start}
+#     exit 0
+# fi
 
-if [ -z $(which docker 2> /dev/null) ]
+if [ -z $(which podman 2> /dev/null) ]
 then
-    # try podman if we dont see docker installed
+    # use docker if we dont see podman installed
     shopt -s expand_aliases
-    alias docker='podman'
-    opts="--privileged "
-    module load gcloud
+    alias podman='docker'
 fi
 
+# TODO move this image to GCR so we dont need DLS specific gcloud creds
 image=gcr.io/diamond-pubreg/controls/python3/s03_utils/epics/edm:latest
 environ="-e DISPLAY=$DISPLAY -e EDMDATAFILES"
 environ="$environ -e EPICS_CA_ADDR_LIST"
 volumes="-v ${thisdir}:/screens -v /tmp:/tmp"
-opts=${opts}"-ti"
+opts=${opts}"--privileged -ti --net host"
 
 set -x
 xhost +local:docker
-docker run ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
+podman run ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
