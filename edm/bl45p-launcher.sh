dfc42d11e4c7e6e551259927486626187e1d1ebb
Giles Knap <giles.knap@diamond.ac.uk>
2022-06-07 08:19:27 +0000

    developing pmac.ibek.defs.yaml and bl45p-mo-ioc-01.yaml

--- edm/bl45p-launcher.sh
+++ edm/bl45p-launcher.sh
@@ -7,17 +7,16 @@ thisdir=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
 # if there is a no cagateway running on p45-ws001 at 172.23.59.64
 # use addresses for all nodes in the p45 beamline instead
-export EPICS_CA_ADDR_LIST:-"172.23.59.101 172.23.59.1"
+export EPICS_CA_ADDR_LIST="172.23.59.101 172.23.59.1"
 
-# if there is a local install of edm run it (deprecated for RHEL8 !!)
-#
-# if [ ! -z $(which edm 2>  /dev/null) ]
-# then
-#     export EDMDATAFILES=$(echo $EDMDATAFILES | sed s+/screens+${thisdir}+g)
-#     echo launching native edm with paths: $EDMDATAFILES
-#     edm -noedit -x ${start}
-#     exit 0
-# fi
+# if there is a local install of edm run it 
+if [ ! -z $(which edm 2>  /dev/null) ]
+then
+    export EDMDATAFILES=$(echo $EDMDATAFILES | sed s+/screens+${thisdir}+g)
+    echo launching native edm with paths: $EDMDATAFILES
+    edm -noedit -x ${start}
+    exit 0
+fi
 
 if [ -z $(which podman 2> /dev/null) ]
 then
--- tests/bl45p-mo-ioc-01.sh
+++ tests/bl45p-mo-ioc-01.sh
@@ -5,12 +5,12 @@ ibek=$root/ibek
 set -e
 
 echo "make schema"
-ibek ioc-schema ${ibek}/{epics,pmac}.ibek.defs.yaml ${ibek}/pmac.ibek.entities.schema.json "${@}"
+ibek ioc-schema ${ibek}/*.ibek.defs.yaml ${ibek}/pmac.ibek.entities.schema.json "${@}"
 
 echo "make helm Chart"
 cd $root
-ibek build-helm bl45p-mo-ioc-99.yaml
+ibek build-helm bl45p-mo-ioc-01.yaml
 
 echo "make ioc startup files"
-cd $root/iocs/bl45p-mo-ioc-99
+cd $root/iocs/bl45p-mo-ioc-01
 ibek build-startup config/ioc.boot.yaml $ibek/*.ibek.defs.yaml --out config/ioc.boot
