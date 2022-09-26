75e558853e205dbc50bb3e8f6b00a635a56fd66a
Giles Knap <giles.knap@diamond.ac.uk>
2022-09-26 12:52:08 +0100

    support new ioc names!

--- helm-push.sh
+++ helm-push.sh
@@ -4,7 +4,7 @@
 # WARNING: use this script for testing but not for deploying 
 # production IOCs. Instead use the CI by pushing a tag to 
 # the main branch (this makes the source for the chart discoverable)
-set -e
+set -xe
 
 IOC_ROOT="$(realpath ${1})"
 # create a tag for todays date and beta number as time of day
@@ -33,22 +33,26 @@ PACKAGE=$(realpath ${NAME}-${TAG}.tgz)
 TMPDIR=$(mktemp -d); cd ${TMPDIR}
 
 # extract the latest version to a temporary folder
-helm pull ${CHART} > out.txt
-LATEST_VERSION=$(sed -n '/^Pulled:/s/.*://p' out.txt)
-echo "LATEST VERSION of ${CHART} is ${LATEST_VERSION}"
-mkdir latest_ioc this_ioc
-tar -xf *tgz -C latest_ioc &> tar1.out
+if helm pull ${CHART} > out.txt; then
+    LATEST_VERSION=$(sed -n '/^Pulled:/s/.*://p' out.txt)
+    echo "LATEST VERSION of ${CHART} is ${LATEST_VERSION}"
+    mkdir latest_ioc this_ioc
+    tar -xf *tgz -C latest_ioc &> tar1.out
 
-# repackage the new IOC with same version as above for direct comparison
-# (packaging reformats the Chart.yaml file so this is the simplest approach)
-helm package "${IOC_ROOT}" --app-version ${LATEST_VERSION} --version ${LATEST_VERSION}
-tar -xf *tgz -C this_ioc &> tar2.out
+    # repackage the new IOC with same version as above for direct comparison
+    # (packaging reformats the Chart.yaml file so this is the simplest approach)
+    helm package "${IOC_ROOT}" --app-version ${LATEST_VERSION} --version ${LATEST_VERSION}
+    tar -xf *tgz -C this_ioc &> tar2.out
 
-# compare the packages and push the new package if it has changed
-if diff -r --exclude Chart.lock latest_ioc this_ioc ; then 
-    echo "NOT PUSHING unchanged IOC ${CHART} version ${LATEST_VERSION}"   
+    # compare the packages and push the new package if it has changed
+    if diff -r --exclude Chart.lock latest_ioc this_ioc ; then 
+        echo "NOT PUSHING unchanged IOC ${CHART} version ${LATEST_VERSION}"   
+    else
+        echo "PUSHING ${CHART} version ${TAG}"   
+        helm push "${PACKAGE}" oci://${HELM_REPO}
+    fi
 else
-    echo "PUSHING ${CHART} version ${TAG}"   
+    # there was no previous version so push this one.
     helm push "${PACKAGE}" oci://${HELM_REPO}
 fi
 
