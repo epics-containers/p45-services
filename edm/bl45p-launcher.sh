829411477250b1cf03fe170a5adb144d24b15a7f
Giles Knap <giles.knap@diamond.ac.uk>
2022-10-10 13:50:08 +0000

    use the containerised edm for launching screens

--- edm/bl45p-launcher.sh
+++ edm/bl45p-launcher.sh
@@ -9,29 +9,25 @@ thisdir=$(realpath $(dirname ${BASH_SOURCE[0]}))
 # use addresses for all nodes in the p45 beamline instead
 export EPICS_CA_ADDR_LIST="172.23.59.101 172.23.59.1 172.23.245.116"
 
-# if there is a local install of edm (that does not throw an error!)
-# TODO fix this to detect RHEL 8 and not use edm 
-if [ edm -h 2>  /dev/null ]
-then
-    export EDMDATAFILES=$(echo $EDMDATAFILES | sed s+/screens+${thisdir}+g)
-    echo launching native edm with paths: $EDMDATAFILES
-    edm -noedit -x ${start}
-    exit 0
-fi
 
-if [ -z $(which podman 2> /dev/null) ]
-then
-    # use docker if we dont see podman installed
-    shopt -s expand_aliases
-    alias podman='docker'
-fi
+export EDMDATAFILES=$(echo $EDMDATAFILES | sed s+/screens+${thisdir}+g)
+echo launching edm with paths: $EDMDATAFILES
+edm -noedit -x ${start}
+exit 0
 
-image=ghcr.io/epics-containers/edm
-environ="-e DISPLAY=$DISPLAY -e EDMDATAFILES"
-environ="$environ -e EPICS_CA_ADDR_LIST"
-volumes="-v ${thisdir}:/screens -v /tmp:/tmp"
-opts=${opts}"--privileged --rm -d --net host"
+# if [ -z $(which podman 2> /dev/null) ]
+# then
+#     # use docker if we dont see podman installed
+#     shopt -s expand_aliases
+#     alias podman='docker'
+# fi
 
-set -x
-xhost +local:docker
-podman run ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
+# image=ghcr.io/epics-containers/edm
+# environ="-e DISPLAY=$DISPLAY -e EDMDATAFILES"
+# environ="$environ -e EPICS_CA_ADDR_LIST"
+# volumes="-v ${thisdir}:/screens -v /tmp:/tmp"
+# opts=${opts}"--privileged --rm -d --net host"
+
+# set -x
+# xhost +local:docker
+# podman run ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
