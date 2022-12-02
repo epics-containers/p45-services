d16ba82580ed259d079e2ba1baad06927e1c8853
Giles Knap <giles.knap@diamond.ac.uk>
2022-12-02 13:45:05 +0000

    add gui for MO90

--- edm/bl45p-launcher.sh
+++ edm/bl45p-launcher.sh
@@ -10,8 +10,7 @@ thisdir=$(realpath $(dirname ${BASH_SOURCE[0]}))
 export EPICS_CA_ADDR_LIST="172.23.59.101 172.23.59.1 172.23.245.116"
 
 # if there is a local install of edm (that does not throw an error!)
-# TODO fix this to detect RHEL 8 and not use edm 
-if [ edm -h 2>  /dev/null ]
+if edm -h &>> /dev/null
 then
     export EDMDATAFILES=$(echo $EDMDATAFILES | sed s+/screens+${thisdir}+g)
     echo launching native edm with paths: $EDMDATAFILES
@@ -34,4 +33,4 @@ opts=${opts}"--privileged --rm -d --net host"
 
 set -x
 xhost +local:docker
-podman run ${environ} ${volumes} ${@} ${opts} ${image} edm -x -noedit ${start}
+podman -r run --rm ${environ} ${volumes} ${@} ${opts} ${image} edm -x ${start}
