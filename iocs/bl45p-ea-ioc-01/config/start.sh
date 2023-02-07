fac2872f7223ac53631eb8a3b638537305f6231a
Giles Knap <giles.knap@diamond.ac.uk>
2023-02-07 13:40:22 +0000

    restore the detector IOC Helm charts

---
+++ iocs/bl45p-ea-ioc-01/config/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+# generic kubernetes IOC startup script
+#
+this_dir=$(realpath $(dirname $0))
+TOP=$(realpath ${this_dir}/..)
+cd ${this_dir}
+
+# add module paths to environment for use in ioc startup script
+source ${SUPPORT}/configure/RELEASE.shell
+
+# if there is a non-zero length config.tz then decompress into config_untar
+if [ -s config.tz ]
+then
+    # decompress the configuration files into config_untar
+    config_dir=${TOP}/config_untar
+
+    mkdir -p ${config_dir}
+    tar -zxvf config.tz -C ${config_dir}
+else
+    config_dir=${TOP}/config
+fi
+
+boot=${config_dir}/ioc.boot
+
+# Update the boot script to work in the directory it resides in
+# using msi MACRO substitution.
+# Output to /tmp for guarenteed writability
+msi -MTOP=${TOP},THIS_DIR=${config_dir},ADCORE=${adcore} ${boot} > /tmp/ioc.boot
+
+exec ${IOC}/bin/linux-x86_64/ioc /tmp/ioc.boot
