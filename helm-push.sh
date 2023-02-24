8c0255145659f2d53948a5e1fab108e401a02ca1
Giles Knap <giles.knap@diamond.ac.uk>
2023-02-24 10:24:39 +0000

    deprecate ibek helm chart generation

--- helm-push.sh
+++ helm-push.sh
@@ -1,64 +1,63 @@
 #!/bin/bash
 
-# NOTE: all beamlines should have CI to push their IOC helm charts
-# WARNING: use this script for testing but not for deploying 
-# production IOCs. Instead use the CI by pushing a tag to 
-# the main branch (this makes the source for the chart discoverable)
 set -xe
 
 IOC_ROOT="$(realpath ${1})"
-# create a tag for todays date and beta number as time of day
-TAG=${2:-$(+%Y.%-m.%-d-b%-H.%M)}
 
 if [ -z "${IOC_ROOT}" ] ; then
   echo "usage: helm-push.sh <ioc root folder> <semvar tag>"
 fi
 
-# Helm Registry in GHCR
-HELM_REPO=ghcr.io/epics-containers
+# TAG defaults to todays date and beta number as time of day
+TAG=${TAG:-$(date +%Y.%-m.%-d-b%-H.%M)}
+# Helm Registry defaults to GHCR
+REGISTRY_ROOT=${REGISTRY_ROOT:-"ghcr.io/epics-containers"}
+# Registry user defaults to USERNAME (default for GHCR)
+CR_USER=${CR_USER:-"USERNAME"}
 
 # extract name from the Chart.yaml
 NAME=$(sed -n '/^name: */s///p' "${IOC_ROOT}/Chart.yaml")
-CHART=oci://${HELM_REPO}/${NAME}
+CHART=oci://${REGISTRY_ROOT}/${NAME}
 
-# Before calling this script: set CR_PAT to a github personal access token 
-# see https://github.com/settings/tokens
-echo $CR_PAT | helm registry login -u USERNAME --password-stdin $HELM_REPO
+# Before calling this script: set CR_TOKEN to an access token fo the
+# target registry.
+# for github GHCR see https://github.com/settings/tokens
+echo ${CR_TOKEN} | helm registry login -u ${CR_USER} --password-stdin $REGISTRY_ROOT
+
+# Temp dir for testing if the helm chart has changed
+TMPDIR=$(mktemp -d); cd ${TMPDIR}
 
 echo "CHECKING deploy of ${IOC_ROOT} to helm repo as version ${TAG} ..."
 helm package "${IOC_ROOT}" -u --app-version ${TAG} --version ${TAG}
 PACKAGE=$(realpath ${NAME}-${TAG}.tgz)
 
-# Temp dir for testing if the helm chart has changed
-TMPDIR=$(mktemp -d); cd ${TMPDIR}
-
 # extract the latest version to a temporary folder
-if helm pull ${CHART} > out.txt; then
+if helm pull ${CHART} &> out.txt; then
+    cat $(realpath out.txt)
     LATEST_VERSION=$(sed -n '/^Pulled:/s/.*://p' out.txt)
     echo "LATEST VERSION of ${CHART} is ${LATEST_VERSION}"
     mkdir latest_ioc this_ioc
-    tar -xf *tgz -C latest_ioc &> tar1.out
+    tar -xf *${LATEST_VERSION}.tgz -C latest_ioc
 
     # repackage the new IOC with same version as above for direct comparison
     # (packaging reformats the Chart.yaml file so this is the simplest approach)
     helm package "${IOC_ROOT}" --app-version ${LATEST_VERSION} --version ${LATEST_VERSION}
-    tar -xf *tgz -C this_ioc &> tar2.out
+    tar -xf *${TAG}.tgz -C this_ioc
 
     # compare the packages and push the new package if it has changed
-    if diff -r --exclude Chart.lock latest_ioc this_ioc ; then 
-        echo "NOT PUSHING unchanged IOC ${CHART} version ${LATEST_VERSION}"   
+    if diff -r --exclude Chart.lock latest_ioc this_ioc ; then
+        echo "NOT PUSHING unchanged IOC ${CHART} version ${LATEST_VERSION}"
     else
-        echo "PUSHING ${CHART} version ${TAG}"   
-        helm push "${PACKAGE}" oci://${HELM_REPO}
+        echo "PUSHING ${CHART} version ${TAG}"
+        helm push "${PACKAGE}" oci://${REGISTRY_ROOT}
     fi
 else
     # there was no previous version so push this one.
-    helm push "${PACKAGE}" oci://${HELM_REPO}
+    helm push "${PACKAGE}" oci://${REGISTRY_ROOT}
 fi
 
 # clean up
 if [ -z ${HELM_PUSH_DEBUG} ] ; then
-    rm "${PACKAGE}"
     rm -r ${TMPDIR}
 else
     echo "helm-push.sh comparison output is in ${TMPDIR}"
