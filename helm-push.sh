2047c857f0c42ad925a9391e6e6a33ac2080aee2
Giles Knap <giles.knap@diamond.ac.uk>
2023-02-27 10:35:29 +0000

    tidy up script into functions

--- helm-push.sh
+++ helm-push.sh
@@ -10,92 +10,118 @@
 # INPUTS
 #  ${1}: (CHART_ROOT) the root folder of the helm chart to be pushed
 #  TAG: the version number to be used for the new helm chart (default: today's date - beta)
-#  REGISTRY_ROOT: the root of the helm registry (default: ghcr.io/epics-containers)
-#  REGISTRY_FOLDER: the folder in the registry (default: the current root folder name)
+#  REGISTRY_FOLDER: the folder in the registry
 #  CR_USER: the username for the registry (default: USERNAME)
-#  CR_TOKEN: the password for the registry (default: None)
+#  CR_TOKEN: the password for the registry
+#  DO_PUSH: if set to true, the chart will be pushed to the registry (default: false)
 
-do_push() {
-    if [[ ${DO_PUSH} == "true" || "true" ]] ; then
-        echo "PUSHING: helm push ${PACKAGE} ${CHART_FOLDER}"
-        helm push "${PACKAGE}" ${CHART_FOLDER}
+
+validate() {
+    # check that the required inputs are present
+    if [[ -z "${1}" || -z "${REGISTRY_FOLDER}" || -z "${CR_TOKEN}" ]] ; then
+        echo "usage: helm-push.sh <chart root folder>"
+        echo "required environment variables:"
+        echo "  REGISTRY_FOLDER: the folder in the registry"
+        echo "  CR_TOKEN: the password for the registry"
+        exit 1
+    fi
+}
+
+
+setup() {
+    # extract the chart name from the Chart.yaml
+    CHART_ROOT="$(realpath ${1})"
+    NAME=$(sed -n '/^name: */s///p' "${CHART_ROOT}/Chart.yaml")
+    CHART=oci://${REGISTRY_FOLDER}/${NAME}
+
+    # TAG defaults to todays date and beta number as time of day
+    if [[ -z "${TAG}" || "${DO_PUSH}" == "false" ]] ; then
+        TAG=$(date +%Y.%-m.%-d-b%-H-%M)
+    fi
+
+    CR_USER=${CR_USER:-"USERNAME"}
+
+    # Temp dir for testing if the helm chart has changed
+    TMPDIR=$(mktemp -d); cd ${TMPDIR}
+}
+
+
+login() {
+    # login to the registry
+    echo ${CR_TOKEN} | helm registry login -u ${CR_USER} --password-stdin http://${REGISTRY_FOLDER}
+}
+
+
+build() {
+    # build the new helm chart
+    # helm lint "${CHART_ROOT}"
+    helm package "${CHART_ROOT}" -u --app-version ${TAG} --version ${TAG}
+    PACKAGE=$(realpath ${NAME}-${TAG}.tgz)
+}
+
+
+compare() {
+    # extract the latest version to a temporary folder
+    if helm pull ${CHART} > out.txt 2>&1; then
+        cat $(realpath out.txt)
+        # the output from helm pull contains the latest version number
+        LATEST_VERSION=$(sed -n '/^Pulled:/s/.*://p' out.txt)
+        echo "LATEST VERSION of ${CHART} is ${LATEST_VERSION}"
+        mkdir latest_chart this_chart
+        tar -xf *${LATEST_VERSION}.tgz -C latest_chart
+        rm *${LATEST_VERSION}.tgz
+
+        # repackage the new chart with same version as above for direct comparison
+        # (packaging reformats the Chart.yaml file so this is the simplest approach)
+        helm package "${CHART_ROOT}" --app-version ${LATEST_VERSION} --version ${LATEST_VERSION}
+        tar -xf *${LATEST_VERSION}.tgz -C this_chart
+
+        # compare the packages and don't push if they are the same
+        if diff -r --exclude Chart.lock latest_chart this_chart ; then
+            echo "chart ${CHART} version ${LATEST_VERSION} is UNCHANGED."
+            DO_PUSH="false"
+        else
+            echo "chart ${CHART} has CHANGED since ${LATEST_VERSION}"
+        fi
     else
-        echo "DRY RUN: helm push ${PACKAGE} ${CHART_FOLDER}"
+        # there was no previous version so push this one.
+        echo "chart ${CHART} version ${LATEST_VERSION} is a NEW chart."
+    fi
+}
+
+
+push() {
+    if [[ ${DO_PUSH} == "true" ]] ; then
+        echo "PUSHING: helm push ${PACKAGE} ${export}"
+        helm push ${PACKAGE} oci://${REGISTRY_FOLDER}
+    else
+        echo "DRY RUN: helm push ${PACKAGE} oci://${REGISTRY_FOLDER}"
         echo "use 'export DO_PUSH=true' to push the beta chart to the registry."
     fi
 }
 
-set -ex
 
-if [[ -z "${1}" ]] ; then
-  echo "usage: helm-push.sh <chart root folder>"
-  exit 1
-fi
-
-CHART_ROOT="$(realpath ${1})"
-
-# TAG defaults to todays date and beta number as time of day
-# GitHub set TAG to branch name for untagged builds so it also sets DO_PUSH to false
-if [[ -z "${TAG}" || "${DO_PUSH}" == "false" ]] ; then
-    TAG=$(date +%Y.%-m.%-d-b%-H-%M)
-else
-    # if a tag was supplied and DO_PUSH is not set to false, then push the chart
-    DO_PUSH=${DO_PUSH:-true}
-fi
-
-# Helm Registry defaults to GHCR
-REGISTRY_ROOT=${REGISTRY_ROOT:-"ghcr.io/epics-containers"}
-# Registry user defaults to USERNAME (default for GHCR)
-CR_USER=${CR_USER:-"USERNAME"}
-# Registry folder should be the name of the repo (default to the current root folder)
-REGISTRY_FOLDER=${REGISTRY_FOLDER:-$(basename $(realpath $(git rev-parse --show-toplevel)))}
-
-# extract name from the Chart.yaml
-NAME=$(sed -n '/^name: */s///p' "${CHART_ROOT}/Chart.yaml")
-CHART_FOLDER=oci://${REGISTRY_ROOT}/${REGISTRY_FOLDER}
-CHART_FOLDER=${CHART_FOLDER%/} # remove trailing slash
-CHART=${CHART_FOLDER}/${NAME}
-
-echo ${CR_TOKEN} | helm registry login -u ${CR_USER} --password-stdin $REGISTRY_ROOT
-
-# Temp dir for testing if the helm chart has changed
-TMPDIR=$(mktemp -d); cd ${TMPDIR}
-
-echo "CHECKING deploy of ${CHART_ROOT} to helm repo as version ${TAG} ..."
-helm package "${CHART_ROOT}" -u --app-version ${TAG} --version ${TAG}
-PACKAGE=$(realpath ${NAME}-${TAG}.tgz)
-
-# extract the latest version to a temporary folder
-if helm pull ${CHART} > out.txt 2>&1; then
-    cat $(realpath out.txt)
-    # the output from helm pull contains the latest version number
-    LATEST_VERSION=$(sed -n '/^Pulled:/s/.*://p' out.txt)
-    echo "LATEST VERSION of ${CHART} is ${LATEST_VERSION}"
-    mkdir latest_chart this_chart
-    tar -xf *${LATEST_VERSION}.tgz -C latest_chart
-    rm *${LATEST_VERSION}.tgz
-
-    # repackage the new chart with same version as above for direct comparison
-    # (packaging reformats the Chart.yaml file so this is the simplest approach)
-    helm package "${CHART_ROOT}" --app-version ${LATEST_VERSION} --version ${LATEST_VERSION}
-    tar -xf *${LATEST_VERSION}.tgz -C this_chart
-
-    # compare the packages and push the new package if it has changed
-    if diff -r --exclude Chart.lock latest_chart this_chart ; then
-        echo "chart ${CHART} version ${LATEST_VERSION} is UNCHANGED."
-    else
-        echo "chart ${CHART} has CHANGED since ${LATEST_VERSION}, deploying version ${TAG}."
-        do_push
-    fi
-else
-    # there was no previous version so push this one.
-    echo "chart ${CHART} version ${LATEST_VERSION} is a NEW chart."
-    do_push
-fi
-
+clean_up() {
 # clean up
-if [[ -z ${HELM_PUSH_DEBUG} ]] ; then
-    rm -r ${TMPDIR}
-else
-    echo "helm-push.sh comparison output is in ${TMPDIR}"
-fi
+    if [[ -z ${HELM_PUSH_DEBUG} ]] ; then
+        rm -r ${TMPDIR}
+    else
+        echo "helm-push.sh comparison output is in ${TMPDIR}"
+    fi
+}
+
+
+
+validate "$@"
+set -ex
+setup "$@"
+login
+build
+compare
+push
+clean_up
+
+
+
+
+
