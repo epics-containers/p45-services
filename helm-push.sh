01e6a4c897d99755ac7afcf181340a3fc87525b7
Giles Knap <giles.knap@diamond.ac.uk>
2023-02-28 11:52:20 +0000

    remove redundant functions

--- helm-push.sh
+++ helm-push.sh
@@ -3,124 +3,111 @@
 # A script for pushing a helm chart to a helm registry
 # it will only push the chart if it has changed since the last version
 #
-# This script is intended to be used in a CI/CD pipeline of both gitlab and github.
-# It can also be run locally - but in this case leave TAG blank and it will use the
-# current date and time as the version number with a beta suffix of current time.
+# This script is for the CI/CD pipeline of both gitlab and github.
+# It can also be run locally - but in this case leave TAG blank and it will
+# use the current date and time as the version number with a beta suffix
+# of current time.
 #
 # INPUTS
 #  ${1}: (CHART_ROOT) the root folder of the helm chart to be pushed
-#  TAG: the version number to be used for the new helm chart (default: today's date - beta)
+#  TAG: the version number for the new helm chart (default: today's date - beta)
 #  REGISTRY_FOLDER: the folder in the registry
 #  CR_USER: the username for the registry (default: USERNAME)
-#  CR_TOKEN: the password for the registry
-#  DO_PUSH: if set to true, the chart will be pushed to the registry (default: false)
+#  CR_TOKEN: the password for the registry (no default)
+#  DO_PUSH: if true, the chart will be pushed to the registry (default: false)
 
 
-validate() {
-    # check that the required inputs are present
-    if [[ -z "${1}" || -z "${REGISTRY_FOLDER}" || -z "${CR_TOKEN}" ]] ; then
-        echo "usage: helm-push.sh <chart root folder>"
-        echo "required environment variables:"
-        echo "  REGISTRY_FOLDER: the folder in the registry"
-        echo "  CR_TOKEN: the password for the registry"
-        exit 1
-    fi
-}
+# validate parameters ##########################################################
+
+set -xe
+
+if [[ -z "${1}" || -z "${REGISTRY_FOLDER}" || -z "${CR_TOKEN}" ]] ; then
+    echo "usage: helm-push.sh <chart root folder>"
+    echo "required environment variables:"
+    echo "  REGISTRY_FOLDER: the folder in the registry"
+    echo "  CR_TOKEN: the password for the registry"
+    exit 1
+fi
 
 
-setup() {
-    # extract the chart name from the Chart.yaml
-    CHART_ROOT="$(realpath ${1})"
-    NAME=$(sed -n '/^name: */s///p' "${CHART_ROOT}/Chart.yaml")
-    CHART=oci://${REGISTRY_FOLDER}/${NAME}
+# setup environment variable ###################################################
 
-    # TAG defaults to todays date and beta number as time of day
-    if [[ -z "${TAG}" || "${DO_PUSH}" == "false" ]] ; then
-        TAG=$(date +%Y.%-m.%-d-b%-H-%M)
-    fi
+# extract the chart name from the Chart.yaml
+CHART_ROOT="$(realpath ${1})"
+NAME=$(sed -n '/^name: */s///p' "${CHART_ROOT}/Chart.yaml")
+CHART=oci://${REGISTRY_FOLDER}/${NAME}
 
-    CR_USER=${CR_USER:-"USERNAME"}
+# TAG defaults to todays date and beta number as time of day
+if [[ -z "${TAG}" || "${DO_PUSH}" == "false" ]] ; then
+    TAG=$(date +%Y.%-m.%-d-b%-H-%M)
+fi
 
-    # Temp dir for testing if the helm chart has changed
-    TMPDIR=$(mktemp -d); cd ${TMPDIR}
-}
+CR_USER=${CR_USER:-"USERNAME"}
 
 
-login() {
-    # login to the registry
-    echo ${CR_TOKEN} | helm registry login -u ${CR_USER} --password-stdin http://${REGISTRY_FOLDER}
-}
+# login ########################################################################
+
+echo ${CR_TOKEN} | helm registry login -u ${CR_USER} \
+    --password-stdin http://${REGISTRY_FOLDER}
 
 
-build() {
-    # build the new helm chart
-    # helm lint "${CHART_ROOT}"
-    helm package "${CHART_ROOT}" -u --app-version ${TAG} --version ${TAG}
-    PACKAGE=$(realpath ${NAME}-${TAG}.tgz)
-}
+# build the new helm chart #####################################################
+
+# helm lint "${CHART_ROOT}"
+helm package "${CHART_ROOT}" -u --app-version ${TAG} --version ${TAG}
+PACKAGE=$(realpath ${NAME}-${TAG}.tgz)
 
 
-compare() {
-    # extract the latest version to a temporary folder
-    if helm pull ${CHART} > out.txt 2>&1; then
-        cat $(realpath out.txt)
-        # the output from helm pull contains the latest version number
-        LATEST_VERSION=$(sed -n '/^Pulled:/s/.*://p' out.txt)
-        echo "LATEST VERSION of ${CHART} is ${LATEST_VERSION}"
-        mkdir latest_chart this_chart
-        tar -xf *${LATEST_VERSION}.tgz -C latest_chart
-        rm *${LATEST_VERSION}.tgz
+# compare ######################################################################
 
-        # repackage the new chart with same version as above for direct comparison
-        # (packaging reformats the Chart.yaml file so this is the simplest approach)
-        helm package "${CHART_ROOT}" --app-version ${LATEST_VERSION} --version ${LATEST_VERSION}
-        tar -xf *${LATEST_VERSION}.tgz -C this_chart
+TMPDIR=$(mktemp -d); cd ${TMPDIR}
 
-        # compare the packages and don't push if they are the same
-        if diff -r --exclude Chart.lock latest_chart this_chart ; then
-            echo "chart ${CHART} version ${LATEST_VERSION} is UNCHANGED."
-            DO_PUSH="false"
-        else
-            echo "chart ${CHART} has CHANGED since ${LATEST_VERSION}"
-        fi
+# extract the latest version to a temporary folder
+if helm pull ${CHART} > out.txt 2>&1; then
+    cat $(realpath out.txt)
+    # the output from helm pull contains the latest version number
+    LATEST_VERSION=$(sed -n '/^Pulled:/s/.*://p' out.txt)
+    echo "LATEST VERSION of ${CHART} is ${LATEST_VERSION}"
+    mkdir latest_chart this_chart
+    tar -xf *${LATEST_VERSION}.tgz -C latest_chart
+    rm *${LATEST_VERSION}.tgz
+
+    # repackage the new chart with same version as above for direct comparison
+    # (packaging reformats the Chart.yaml file so this is the simplest approach)
+    helm package "${CHART_ROOT}" --app-version ${LATEST_VERSION} \
+        --version ${LATEST_VERSION}
+    tar -xf *${LATEST_VERSION}.tgz -C this_chart
+
+    # compare the packages and don't push if they are the same
+    if diff -r --exclude Chart.lock latest_chart this_chart ; then
+        echo "chart ${CHART} version ${LATEST_VERSION} is UNCHANGED."
+        DO_PUSH="false"
     else
-        # there was no previous version so push this one.
-        echo "chart ${CHART} version ${LATEST_VERSION} is a NEW chart."
+        echo "chart ${CHART} has CHANGED since ${LATEST_VERSION}"
     fi
-}
+else
+    # there was no previous version so push this one.
+    echo "chart ${CHART} version ${LATEST_VERSION} is a NEW chart."
+fi
 
 
-push() {
-    if [[ ${DO_PUSH} == "true" ]] ; then
-        echo "PUSHING: helm push ${PACKAGE} ${export}"
-        helm push ${PACKAGE} oci://${REGISTRY_FOLDER}
-    else
-        echo "DRY RUN: helm push ${PACKAGE} oci://${REGISTRY_FOLDER}"
-        echo "use 'export DO_PUSH=true' to push the beta chart to the registry."
-    fi
-}
+# push #########################################################################
+
+if [[ ${DO_PUSH} == "true" ]] ; then
+    echo "PUSHING: helm push ${PACKAGE} ${export}"
+    helm push ${PACKAGE} oci://${REGISTRY_FOLDER}
+else
+    echo "DRY RUN: helm push ${PACKAGE} oci://${REGISTRY_FOLDER}"
+    echo "use 'export DO_PUSH=true' to push the beta chart to the registry."
+fi
 
 
-clean_up() {
-# clean up
-    if [[ -z ${HELM_PUSH_DEBUG} ]] ; then
-        rm -r ${TMPDIR}
-    else
-        echo "helm-push.sh comparison output is in ${TMPDIR}"
-    fi
-}
-
-
-
-validate "$@"
-set -ex
-setup "$@"
-login
-build
-compare
-push
-clean_up
-
+# clean up #####################################################################
+if [[ -z ${HELM_PUSH_DEBUG} ]] ; then
+    rm -r ${TMPDIR}
+else
+    echo "helm-push.sh comparison output is in ${TMPDIR}"
+fi
 
 
 
