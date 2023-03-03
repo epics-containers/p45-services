04b250a456184dca676439a713877376498d4b96
Giles Knap <giles.knap@diamond.ac.uk>
2023-03-03 14:41:35 +0000

    updates to latest epics-base and ioc-pmac

--- iocs/bl45p-mo-ioc-01/config/start.sh
+++ iocs/bl45p-mo-ioc-01/config/start.sh
@@ -3,30 +3,35 @@
 #
 # generic kubernetes IOC startup script
 #
+
+set -x -e
+
+# environment setup
 this_dir=$(realpath $(dirname $0))
 TOP=$(realpath ${this_dir}/..)
 cd ${this_dir}
+config_dir=${TOP}/config
 
 # add module paths to environment for use in ioc startup script
 source ${SUPPORT}/configure/RELEASE.shell
 
-# if there is a non-zero length config.tz then decompress into config_untar
-if [ -s config.tz ]
-then
-    # decompress the configuration files into config_untar
-    config_dir=${TOP}/config_untar
+# setup filenames for boot scripts
+startup_src=${config_dir}/ioc.boot.yaml
+boot_src=${config_dir}/ioc.boot
+db_src=/tmp/make_db.sh
+boot=/tmp/$(basename ${boot_src})
+db=/tmp/ioc.db
 
-    mkdir -p ${config_dir}
-    tar -zxvf config.tz -C ${config_dir}
-else
-    config_dir=${TOP}/config
+# If there is a yaml ioc description then generate the startup and DB.
+# Otherwise assume the starup script ioc.boot is provided in the config folder.
+if [ -f ${startup_src} ] ; then
+    # get ibek support files from this ioc and all it's support modules
+    defs=/ctools/*/*.ibek.support.yaml
+    ibek build-startup ${startup_src} ${defs} --out ${boot} --db-out ${db_src}
 fi
 
-boot=${config_dir}/ioc.boot
-
-# Update the boot script to work in the directory it resides in
-# using msi MACRO substitution.
-# Output to /tmp for guarenteed writability
-msi -MTOP=${TOP},THIS_DIR=${config_dir},ADCORE=${adcore} ${boot} > /tmp/ioc.boot
-
-exec ${IOC}/bin/linux-x86_64/ioc /tmp/ioc.boot
+# build expanded database using the db_src shell script
+if [ -f ${db_src} ]; then
+    bash ${db_src} > ${db}
+fi
+exec ${IOC}/bin/linux-x86_64/ioc ${boot}
--- iocs/bl45p-mo-ioc-90/config/start.sh
+++ iocs/bl45p-mo-ioc-90/config/start.sh
@@ -3,26 +3,17 @@
 #
 # generic kubernetes IOC startup script
 #
-this_dir=$(realpath $(dirname $0))
-TOP=$(realpath ${this_dir}/..)
-cd ${this_dir}
 
 set -x -e
 
+# environment setup
+this_dir=$(realpath $(dirname $0))
+TOP=$(realpath ${this_dir}/..)
+cd ${this_dir}
+config_dir=${TOP}/config
+
 # add module paths to environment for use in ioc startup script
 source ${SUPPORT}/configure/RELEASE.shell
-
-# if there is a non-zero length config.tz then decompress into config_untar
-if [ -s config.tz ]
-then
-    # decompress the configuration files into config_untar (/tmp is writeable)
-    config_dir=/tmp/config_untar
-
-    mkdir -p ${config_dir}
-    tar -zxvf config.tz -C ${config_dir}
-else
-    config_dir=${TOP}/config
-fi
 
 # setup filenames for boot scripts
 startup_src=${config_dir}/ioc.boot.yaml
@@ -34,12 +25,8 @@ db=/tmp/ioc.db
 # If there is a yaml ioc description then generate the startup and DB.
 # Otherwise assume the starup script ioc.boot is provided in the config folder.
 if [ -f ${startup_src} ] ; then
-    # get ibek defs files from this ioc and all support modules and this IOC
-    # defs="${SUPPORT}/*/ibek/*.ibek.defs.yaml ${TOP}/ibek/*.ibek.defs.yaml"
-
-    # TODO - for early development we ship all latest def files in ioc/ibek
-    # These Should Assets published by epics-modules and ioc-pmac
-    defs=${TOP}/ibek/*.ibek.defs.yaml
+    # get ibek support files from this ioc and all it's support modules
+    defs=/ctools/*/*.ibek.support.yaml
     ibek build-startup ${startup_src} ${defs} --out ${boot} --db-out ${db_src}
 fi
 
