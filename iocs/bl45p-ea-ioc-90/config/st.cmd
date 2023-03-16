e865db8577aca4bfcdb28bd1e7adbc5f27c16372
Giles Knap <giles.knap@diamond.ac.uk>
2023-03-16 09:12:51 +0000

    first fully working RTEMS IOC bl45p-ea-ioc-90

--- iocs/bl45p-ea-ioc-90/config/st.cmd
+++ iocs/bl45p-ea-ioc-90/config/st.cmd
@@ -1,5 +1,4 @@
-# A dummy IOC startup script for verifying that a generic IOC binary
-# has been built correctly.
+# RTEMS Test IOC BL45P-EA-IOC-90
 
 on 'error' 'break'
 
@@ -7,8 +6,12 @@ epicsEnvSet("EPICS_CA_REPEATER_PORT","7065")
 epicsEnvSet("EPICS_CA_SERVER_PORT","7064")
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","3000000")
 epicsEnvSet("EPICS_TS_MIN_WEST","0")
+epicsEnvSet("IOCSH_PS1","bl45p-ea-ioc-90 > ")
 
-dbLoadDatabase "dbd/ioc.dbd"
+dbLoadDatabase "/iocs/bl45p/bl45p-ea-ioc-90/dbd/ioc.dbd"
 ioc_registerRecordDeviceDriver(pdbbase)
 epicsEnvSet("PRINTDEBUG", "3")
+
+dbLoadRecords("/iocs/bl45p/bl45p-ea-ioc-90/config/ioc.db")
+
 iocInit
