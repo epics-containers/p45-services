8ae6f2348426e1dcc7309d8a2a932f6dd1f7f124
Giles Knap <giles.knap@diamond.ac.uk>
2023-04-24 10:14:01 +0000

    remove redundant script call in ci_build

--- ci_build.sh
+++ ci_build.sh
@@ -8,9 +8,6 @@ set -ex
 # the included busybox diff does not support --exclude
 apk --no-cache add diffutils
 
-# tar up the ioc source if needed (TODO temp workaround until ibek builds IOC contents)
-./tar_config_src.sh
-
 # build and push the helm chart for each ioc
 for ioc_root in $(ls -d iocs/*/) ; do
     ./helm-push.sh ${ioc_root} ;
