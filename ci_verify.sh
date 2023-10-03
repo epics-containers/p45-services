677acb974d44829a5a1c0418a4a9040aef61808e
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-03 16:35:07 +0100

    add CI using ec

---
+++ ci_verify.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# CI framework agnostic to verify all the IOC instances specified in the repo
+
+THIS_DIR=$(dirname ${0})
+set -ex
+
+pip install --upgrade "epics-containers-cli>=2.0.0"
+
+# setup environment for epics-containers-cli (ec) to work with BL45P
+source ${THIS_DIR}/bl45p
+
+for ioc in iocs/*
+do
+    if [ ! -d "${ioc}/config" ]; then
+        continue
+    fi
+    
+    # verify that the instance can generate a schema
+    ec dev launch --target runtime ${ioc} --execute \
+    'ibek ioc generate-schema /epics/ioc-adaravis/ibek-support/*/*.ibek.support.yaml'
+    
+    # verify that the instance can launch its IOC
+    
+    # put the values.yaml file a test config directory with basic startup script
+    cp -r ${ioc}/values.yaml ci_test/
+    # launch the generic IOC pointing at that config
+    container=$(ec dev launch --target runtime ci_test --args '-dit')
+    podman stop ${container} -t0
+    
+done
--- iocs/generic-ioc-instance/config/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo this is the first fixed helm chart generic ioc deployment
-
-while true; do sleep 2; done;
