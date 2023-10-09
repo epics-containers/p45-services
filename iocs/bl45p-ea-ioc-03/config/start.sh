226af3c743a39af5e085689fd274a1c043cb6173
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-09 15:23:58 +0100

    final panda instance

--- iocs/bl45p-ea-ioc-03/config/start.sh
+++ iocs/bl45p-ea-ioc-03/config/start.sh
@@ -5,4 +5,3 @@ mkdir -p /epics/links/opi
 # Start the Panda soft IOC application
 pandablocks-ioc softioc bl45p-mo-panda-01 BL45P-EA-IOC-03 --clear-bobfiles --screens-dir /epics/links/opi
 
-sleep 1000
