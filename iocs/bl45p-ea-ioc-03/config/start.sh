4dd56a94f323d2f878ceb01c878d4e524c352dbd
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-09 14:44:38 +0100

    add panda soft IOC support and instance

---
+++ iocs/bl45p-ea-ioc-03/config/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create the OPI directory
+mkdir -p /epics/links/opi
+# Start the Panda soft IOC application
+pandablocks-ioc softioc bl45p-mo-panda-01 BL45P-EA-IOC-03 --clear-bobfiles --screens-dir /epics/links/opi
+
+sleep 1000
