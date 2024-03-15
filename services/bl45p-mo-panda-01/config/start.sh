58a5cbb296965ab10b5098e654aa6044d0011275
Giles Knap <giles.knap@diamond.ac.uk>
2024-03-15 10:22:05 +0000

    add motion and panda iocs

---
+++ services/bl45p-mo-panda-01/config/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pandablocks-ioc softioc 172.23.59.50 BL45P-MO-PANDA-01 --clear-bobfiles --screens-dir /epics/opi
---
+++ services/bl45p-mo-panda-01/config/stop.sh
@@ -0,0 +1,2 @@
+# a stop script for K8S to call when closing the container
+exit 0
